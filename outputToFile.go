a30d1691e02fad3b73d1ea2ed20b106347d1fbf2
Timothy Ododo <timothyododo@gmail.com>
2026-06-08 16:03:08 +0100

    check for wrong file format for question 2

--- outputToFile.go
+++ outputToFile.go
@@ -1,5 +1,14 @@
 package main
 
-func outputToFile(inputFile string, outputFile string) (error, string) {
+import (
+	"fmt"
+	"strings"
+)
 
+func outputToFile(inputFile string, outputFile string) (error, string) {
+	// check for wrong file format.
+	if !(strings.HasSuffix(inputFile, ".txt") && strings.HasSuffix(outputFile, ".txt")) {
+		return fmt.Errorf("please enter the right file formats: input and output file must be a text file which ends with '.txt'"), ""
+
+	}
 }
