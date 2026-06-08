408093293cad12e48f4c35699dab0f378c47aab3
Timothy Ododo <timothyododo@gmail.com>
2026-06-08 16:13:58 +0100

    reaading input file for question 2

--- outputToFile.go
+++ outputToFile.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -9,6 +10,16 @@ func outputToFile(inputFile string, outputFile string) (error, string) {
 	// check for wrong file format.
 	if !(strings.HasSuffix(inputFile, ".txt") && strings.HasSuffix(outputFile, ".txt")) {
 		return fmt.Errorf("please enter the right file formats: input and output file must be a text file which ends with '.txt'"), ""
+
+	} else {
+		// read input file
+		inputContent, err := os.ReadFile(outputFile)
+		// check and capture reading input file error
+		if err != nil {
+			return fmt.Errorf("unable to read file: %v", err), ""
+		}
+
+		inputContentString := string(inputContent)
 
 	}
 }
